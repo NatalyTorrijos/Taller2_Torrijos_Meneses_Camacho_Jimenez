48b7ece63aa1d74340f8df8a16b84561996220d3
GIGASAMCC23 <samuel.camacho@uao.edu.co>
2025-10-03 11:37:06 -0500

    animacion salto/caidaa
    se agrego animacion de salto y caida con la deteccion de las plataforma por medio de tag

--- Taller2_Unity/Assets/Scripts/PlayerMove.cs
+++ Taller2_Unity/Assets/Scripts/PlayerMove.cs
@@ -8,8 +8,11 @@ public class MoverPlayer : MonoBehaviour
     private float horizontal;
     public float speed;
     public float jumpForce;
-    private bool Grounded;
     private Vector3 initialScale;
+
+    private bool isGrounded;
+    private int jumpCount;
+    public int maxJumps = 2;
 
     void Start()
     {
@@ -29,25 +32,64 @@ public class MoverPlayer : MonoBehaviour
 
         animator.SetBool("running", horizontal != 0.0f);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (isGrounded) jumpCount = 0;
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount < maxJumps)
         {
             Jump();
+            jumpCount++;
         }
 
         if (Input.GetMouseButtonDown(0))
         {
             animator.SetTrigger("attack");
         }
+
+        if (!isGrounded)
+        {
+            if (rb.linearVelocity.y > 1f)
+            {
+                animator.SetBool("jumping", true);
+                animator.SetBool("falling", false);
+            }
+            else if (rb.linearVelocity.y < -1f)
+            {
+                animator.SetBool("jumping", false);
+                animator.SetBool("falling", true);
+            }
+        }
+        else
+        {
+            animator.SetBool("jumping", false);
+            animator.SetBool("falling", false);
+        }
     }
 
     private void Jump()
     {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
         rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
     private void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            isGrounded = false;
+        }
     }
 
     internal void RecibeDanio(Vector2 direcciondanio, int v)
