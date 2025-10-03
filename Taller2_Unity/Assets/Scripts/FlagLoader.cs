869bb319dc4d835bc74ec39d75ac4889f9d9917b
NatalyTorrijos <167800606+NatalyTorrijos@users.noreply.github.com>
2025-10-03 14:21:09 -0500

    panel fina

--- Taller2_Unity/Assets/Scripts/FlagLoader.cs
+++ Taller2_Unity/Assets/Scripts/FlagLoader.cs
@@ -4,13 +4,15 @@ using UnityEngine.SceneManagement;
 public class FlagLoader : MonoBehaviour
 {
     public string sceneToLoad;
-
+    public Timer timerPanel;
     private void OnTriggerEnter2D(Collider2D other)
     {
 
         if (other.CompareTag("Player"))
         {
             SceneManager.LoadScene(sceneToLoad);
+
+            GameManager.Instance.TotalTime(timerPanel.GetCurrentTime());
         }
     }
 }
--- Taller2_Unity/Assets/Scripts/MetricasFinales.cs
+++ Taller2_Unity/Assets/Scripts/MetricasFinales.cs
@@ -1,24 +1,27 @@
+using TMPro;
 using UnityEngine;
 
 public class MetricasFinales : MonoBehaviour
 {
-    public GameObject panelResultados; 
-
+    public TMP_Text tiempoText;
+    public TMP_Text monedasText;
+    public TMP_Text pocionesText;
+    public TMP_Text vidasText;
+    public TMP_Text puntosText;
     void Start()
     {
-        
-        if (panelResultados != null)
-            panelResultados.SetActive(false);
+        var gm = GameManager.Instance;
+
+        tiempoText.text =  gm.GlobalTime.ToString("F2");
+        monedasText.text = gm.scoreCoin.ToString();
+        pocionesText.text =  gm.scorePoison.ToString();
+        vidasText.text =  gm.playerLives.ToString();
+        puntosText.text =  (gm.scoreCoin + gm.scorePoison).ToString();
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    // Update is called once per frame
+    void Update()
     {
         
-        if (other.CompareTag("Player"))
-        {
-            if (panelResultados != null)
-                panelResultados.SetActive(true);
-
-        }
     }
 }
